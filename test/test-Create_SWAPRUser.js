27d4a48452931aa0d8d2f84415210f6e99a53fa4
Eric Christie <echristie3@gatech.edu>
2016-10-05 20:46:02 -0400

    added more tests for Create SWAPRUser and revised the old tests

--- test/test-Create_SWAPRUser.js
+++ test/test-Create_SWAPRUser.js
@@ -16,14 +16,17 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
   it('returns username, first_name, last_name, and email when given valid input'
       + 'for those fields along with a password', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_6",
+      "first_name": "User",
+      "last_name": "Six",
+      "email": "test6@email.com",
+      "password": "password6"
     };
     var expectedResponseBody = {
-
+      "username": "user_6",
+      "first_name": "User",
+      "last_name": "Six",
+      "email": "test6@email.com"
     };
     request
       .post('/swaprusers')
@@ -38,13 +41,13 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
       });
   });
 
-  it('returns an error when duplicate username is input', function(done) {
+  it('returns an error when a duplicate username is input', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_1",
+      "first_name": "Duplicate",
+      "last_name": "Username",
+      "email": "test1duplicate@email.com",
+      "password": "password1duplicate"
     };
     request
       .post('/swaprusers')
@@ -52,13 +55,13 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
       .end(expectErrorResponse(err, res));
   });
 
-  it('returns an error when duplicate email is input', function(done) {
+  it('returns an error when a duplicate email is input', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_7",
+      "first_name": "Seventh",
+      "last_name": "User",
+      "email": "test1@email.com",
+      "password": "password7"
     };
     request
       .post('/swaprusers')
@@ -68,14 +71,17 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('allows duplicate passwords', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_7",
+      "first_name": "Seventh",
+      "last_name": "User",
+      "email": "test7@email.com",
+      "password": "password1"
     };
     var expectedResponseBody = {
-
+      "username": "user_7",
+      "first_name": "Seventh",
+      "last_name": "User",
+      "email": "test7@email.com"
     };
     request
       .post('/swaprusers')
@@ -90,13 +96,81 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
       });
   });
 
+  it('allows duplicate first_names', function(done) {
+    var requestBody = {
+      "username": "user_8",
+      "first_name": "User",
+      "last_name": "Eight",
+      "email": "test8@email.com",
+      "password": "password8"
+    };
+    var expectedResponseBody = {
+      "username": "user_8",
+      "first_name": "User",
+      "last_name": "Eight",
+      "email": "test8@email.com"
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .expect(201, expectedResponseBody)
+      .expect('Content-Type', 'application/json')
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+
+  it('allows duplicate last_names', function(done) {
+    var requestBody = {
+      "username": "user_9",
+      "first_name": "Ninth",
+      "last_name": "User",
+      "email": "test9@email.com",
+      "password": "password9"
+    };
+    var expectedResponseBody = {
+      "username": "user_9",
+      "first_name": "Ninth",
+      "last_name": "User",
+      "email": "test9@email.com"
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .expect(201, expectedResponseBody)
+      .expect('Content-Type', 'application/json')
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+
+  it('requires that a username be provided', function(done) {
+    var requestBody = {
+      "username": "",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
+  });
+
   it('requires that a first_name be provided', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -106,11 +180,40 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('requires that a last_name be provided', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "",
+      "email": "test0@email.com",
+      "password": "crocodile"
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
+  });
+
+  it('requires that an email address be provided', function(done) {
+    var requestBody = {
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "",
+      "password": "crocodile"
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
+  });
+
+  it('requires that the email address input take the form of an email address',
+    function(done) {
+    var requestBody = {
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "Not An Email Address",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -120,11 +223,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('returns an error when given an empty password field', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": ""
     };
     request
       .post('/swaprusers')
@@ -134,11 +237,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept numerical values as inputs for username', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": 0,
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -148,11 +251,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept boolean values as inputs for username', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": false,
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -162,11 +265,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept arrays as inputs for username', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": ["array", "of", "strings"],
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -176,11 +279,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept numerical values as inputs for first_name', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": 0,
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -190,11 +293,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept boolean values as inputs for first_name', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": true,
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -204,11 +307,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept arrays as inputs for first_name', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": ["array", "of", "strings"],
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -218,11 +321,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept numerical values as inputs for last_name', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": 0,
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -232,11 +335,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept boolean values as inputs for last_name', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": true,
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -246,11 +349,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept arrays as inputs for last_name', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": ["array", "of", "strings"],
+      "email": "test0@email.com",
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -260,11 +363,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept numerical values as inputs for email', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": 0,
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -274,11 +377,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept boolean values as inputs for email', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": false,
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -288,11 +391,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept arrays as inputs for email', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": ["array", "of", "strings"],
+      "password": "crocodile"
     };
     request
       .post('/swaprusers')
@@ -302,11 +405,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept numerical values as inputs for password', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": 0
     };
     request
       .post('/swaprusers')
@@ -316,11 +419,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept boolean values as inputs for password', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": true
     };
     request
       .post('/swaprusers')
@@ -330,11 +433,11 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
 
   it('does not accept arrays as inputs for password', function(done) {
     var requestBody = {
-      "username": ,
-      "first_name": ,
-      "last_name": ,
-      "email": ,
-      "password":
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": ["array", "of", "strings"]
     };
     request
       .post('/swaprusers')
@@ -342,107 +445,69 @@ describe('Create SWAPRUser', function testCreateSWAPRUser() {
       .end(expectErrorResponse(err, res));
   });
 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  it('should take username, first_name, last_name, email, and password as inputs'
-    + 'and respond to valid requests by returning all inputs except for password', function (done) {
-    request({
-      method: 'POST',
-      url: URL,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: "{'username': 'student0', 'first_name': 'Student', 'last_name': 'Zero', 'email': 'student0@example.com', 'password': 'studentpass'}"
-    }, function (error, response, body) {
-      var code = response.statusCode;
-      var contentType = reponse.headers['Content-Type'];
-      var result = JSON.parse(body);
-      assert.equal(code, 201, "Status code does not match.");
-      assert.equal(contentType, 'application/json', "Content-Type does not match.");
-      var expected = {
-        "id": 5,
-        "username": "student0",
-        "first_name": "Student",
-        "last_name": "Zero",
-        "email": "student0@example.com"
-      };
-      assert.equal(result, expected, "Response body does not match.");
-      done();
-    });
+  it('does not allow username to be undefined', function(done) {
+    var requestBody = {
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": ["array", "of", "strings"]
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
   });
 
-
-  it('should return an error when a duplicate username is input', function (done) {
-    request({
-      method: 'POST',
-      url: URL,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: "{'username': 'user_1', 'first_name': 'Joe', 'last_name': 'Userman', 'email': 'duplicateUser@email.com', 'password': 'randompassword'}"
-    }, function (error, response, body) {
-      var code = response.statusCode;
-      var contentType = reponse.headers['Content-Type'];''
-      var result = JSON.parse(body);
-      assert.equal(code, 400, "Status code does not match.");
-      assert.equal(contentType, 'application/json', "Content-Type does not match.");
-      var expected = {
-        "error": "django user with username already exists"
-      };
-      assert.equal(result, expected, "Response body does not match.");
-      done();
-    });
+  it('does not allow first_name to be undefined', function(done) {
+    var requestBody = {
+      "username": "user_0",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+      "password": ["array", "of", "strings"]
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
   });
 
-
-  it('should return an error when a duplicate email is input', function(done) {
-    request({
-      method: 'POST',
-      url: URL,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: "{'username': 'user_7', 'first_name': 'Duplicate', 'last_name': 'User', 'email': 'test1@email.com', 'password': 'randompassword'}"
-    }, function (error, response, body) {
-      var code = response.statusCode;
-      var contentType = reponse.headers['Content-Type'];
-      var result = JSON.parse(body);
-      assert.equal(code, 400, "Status code does not match.");
-      assert.equal(contentType, 'application/json', "Content-Type does not match.");
-      var expected = {
-        "error": "django user with email already exists"
-      };
-      assert.equal(result, expected, "Response body does not match.");
-      done();
-    });
+  it('does not allow last_name to be undefined', function(done) {
+    var requestBody = {
+      "username": "user_0",
+      "first_name": "Mister",
+      "email": "test0@email.com",
+      "password": ["array", "of", "strings"]
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
   });
 
+  it('does not allow email to be undefined', function(done) {
+    var requestBody = {
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "password": ["array", "of", "strings"]
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
+  });
 
-  it('should allow duplicate passwords', function (done) {
-    request({
-      method: 'POST',
-      url: URL,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: "{'username': 'student1', 'first_name': 'Student', 'last_name': 'One', 'email': 'student1@example.com', 'password': 'studentpass'}"
-    }, function (error, response, body) {
-      var code = response.statusCode;
-      var contentType = reponse.headers['Content-Type'];
-      var result = JSON.parse(body);
-      assert.equal(code, 201, "Status code does not match.");
-      assert.equal(contentType, 'application/json', "Content-Type does not match.");
-      var expected = {
-        "id": 6,
-        "username": "student1",
-        "first_name": "Student",
-        "last_name": "One",
-        "email": "student1@example.com"
-      };
-      assert.equal(result, expected, "Response body does not match.");
-      done();
-    });
+  it('does not allow password to be undefined', function(done) {
+    var requestBody = {
+      "username": "user_0",
+      "first_name": "Mister",
+      "last_name": "Zero",
+      "email": "test0@email.com",
+    };
+    request
+      .post('/swaprusers')
+      .send(requestBody)
+      .end(expectErrorResponse(err, res));
   });
 
 });

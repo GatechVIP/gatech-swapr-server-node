3f6169860a67d55031e2720488881bc088d4382c
Ayan Das <ayandas@lawn-143-215-49-143.lawn.gatech.edu>
2016-11-04 00:28:04 -0400

    Added some authentication strategies

--- swapr_server/config/passport.js
+++ swapr_server/config/passport.js
@@ -6,7 +6,7 @@ var bcrypt = require('bcrypt-nodejs');
 
 passport.use(new LocalStrategy(
     function(username, password, done) {
-        bcrypt.genSalt(5, function(err, salt) {
+        /*bcrypt.genSalt(5, function(err, salt) {
             if (err) return done(err);
             bcrypt.hash(password, salt, null, function(error, hash) {
                 if (error) return done(error);
@@ -20,8 +20,36 @@ passport.use(new LocalStrategy(
                     }
                 })
             })
+        })*/
+        db.all("SELECT * FROM id_map WHERE username = '" + username + "'", function(err, row) {
+            if (err) return done(error);
+            if (!row) {
+                return done(null, false, {message: 'No such username'});
+            } else {
+                bcrypt.compare(password, row.pwd_hash, function(err, res) {
+                    if (res) {
+                        var user = {username: row.username, token: row.token};
+                        return done(null, user);
+                    } else {
+                        return done(null, false, {message: 'Incorrect password'});
+                    }
+                })
+            }
         })
     }
 ));
 
+
+passport.use(new BearerStrategy(
+    function(token, done) {
+        db.all("SELECT * FROM id_map WHERE token = '" + token + "'", function(err, results) {
+            if (err) { return done(err); }
+            if (!results) {
+                return done(null, false, {message: 'Unknown user'});
+            } else {
+                return done(null, user, { scope: 'all' });
+            }
+        })
+    }
+));
 module.exports = passport;

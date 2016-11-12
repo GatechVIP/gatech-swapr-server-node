221729a5a54f59e79a723840f3f441079ffd292d
Eric <ericjchristie@gmail.com>
2016-11-12 14:16:33 -0500

    testing different ways of requiring the database

---
+++ db/sqlite.js
@@ -0,0 +1,15 @@
+// this starts an in-memory database and creates all the tables for it
+var db = require('./sqliteSetup');
+
+//populate the role_map table
+db.run("INSERT INTO role_map (role_id, role) VALUES (0, 'instructor')");
+db.run("INSERT INTO role_map (role_id, role) VALUES (1, 'student')");
+
+module.exports = ;
+
+console.log('Current Module: ' + module);
+console.log('Main Module: ' + require.main);
+console.log('Module Children: ' + module.children);
+console.log('Module Exports: ' + module.exports);
+console.log('Module Parent: ' + module.parent);
+console.log('Is module loaded? ' + module.loaded);
---
+++ models/dummy.js
@@ -0,0 +1,9 @@
+
+
+
+console.log('Current Module: ' + module);
+console.log('Main Module: ' + require.main);
+console.log('Module Children: ' + module.children);
+console.log('Module Exports: ' + module.exports);
+console.log('Module Parent: ' + module.parent);
+console.log('Is module loaded? ' + module.loaded);

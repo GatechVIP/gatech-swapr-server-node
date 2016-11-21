3d6cc9c6aa363e618d85a647532b24f20f9df339
Eric <ericjchristie@gmail.com>
2016-11-21 13:21:03 -0500

    testing out ways of making the database work

--- db/sqliteSetup.js
+++ db/sqliteSetup.js
@@ -1,5 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:'); //create db in memory, volatile!
+var db = new sqlite3.cached.Database(':memory:'); //create db in memory, volatile!
 var exists = false; //TODO: when we write the db to disk, then we need to check if it exists
 
 module.exports = db;

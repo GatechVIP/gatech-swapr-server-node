4d57e2638a6a8b0ea206500e1445016bb7ffe8ca
Eric <ericjchristie@gmail.com>
2016-11-22 10:14:38 -0500

    creating file for handing sql queries

--- db/sqlHandler.js
+++ db/sqlHandler.js
@@ -1,1 +1,20 @@
-var db = require('./sqliteSetup');
+// var db = require('./sqliteSetup');
+
+/* get the database object */
+
+// this will work as long as the server is always started via `npm start`
+//  which does `node ./bin/www` which in turn calls app.js
+
+// thanks to module caching, some of the concerns about creating multiple
+//  database instances may not be an issue
+// due to module caching, all files in a single directory that require
+//  sqliteSetup.js should be given the same module object, and the code in
+//  sqliteSetup.js should only be executed once
+// this might also apply to files in other directories that require
+//  sqliteSetup.js, but I am unsure
+
+
+// according to the modules page in the api docs on nodejs.org:
+//  "every call to require('foo') will get exactly the same object returned,
+//  if it would resolve to the same file"
+

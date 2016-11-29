5ea2ee528f7bbc936d7dfdc49439ce1bdcfc4a95
Eric <ericjchristie@gmail.com>
2016-11-29 10:02:34 -0500

    hard-coding ORM for user model

--- models/user.js
+++ models/user.js
@@ -18,20 +18,68 @@ var db = require('../db/sqliteSetup');
 //  the property names must match the column names from the id_map table
 // also the controllers should probably validate the objects that are input
 //  into these functions
-module.exports.insert = function(usr, callback) {
-  var cols = Object.keys(usr);
-  var vals = Object.values(usr);
-  db.all("INSERT INTO id_map (" + cols.toString() + ") VALUES (" + vals.toString() + ")");
+// the callback functions have the parameters result and error
+
+module.exports.insert = function(user, callback) {
+  var result = null;
+  var error = null;
+
+  var cols = Object.keys(user);
+  var vals = Object.values(user);
+  db.run("INSERT INTO id_map (" + cols.toString() +
+    ") VALUES (" + vals.toString() + ")", function(err) {
+    if (err) {
+      error = err;
+    } else {
+
+    }
+  });
 };
 
-module.exports.update = function(usr, callback) {
+// attributes for an existing entry in the id_map table are specified in the
+//  oldUser object while attributes to be changed/added are specified in newUser
+// newUser should only contain a single attribute to be updated
+module.exports.update = function(oldUser, newUser, callback) {
+  var result = null;
+  var error = null;
 
+  var oldCols = Object.keys(oldUser);
+  var oldVals = Object.values(oldUser);
+  var newCols = Object.keys(newUser);
+  var newVals = Object.values(newUser);
+  db.run("UPDATE id_map SET " + , function(err) {
+    if (err) {
+      error = err;
+    } else {
+
+    }
+  });
 };
 
-module.exports.select = function(usr, callback) {
+module.exports.select = function(user, callback) {
+  var result = null;
+  var error = null;
 
+  var cols = Object.keys(user);
+  var vals = Object.values(user);
+  db.all(, function(err, rows) {
+    if (err) {
+      error = err;
+    }
+  })
 };
 
-module.exports.delete = function(usr, callback) {
+module.exports.delete = function(user, callback) {
+  var result = null;
+  var error = null;
 
+  var cols = Object.keys(user);
+  var vals = Object.values(user);
+  db.run(, function(err) {
+    if (err) {
+      error = err;
+    } else {
+
+    }
+  });
 };

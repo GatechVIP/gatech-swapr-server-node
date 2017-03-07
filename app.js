60b625fb645ab6b75c32b397bde4a302be1fa3b2
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2017-03-07 17:23:34 -0500

    clean up commented out code

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var sqlDB = require('./db/sqliteSetup');
 var sqlite3 = require('sqlite3').verbose();
-//var sqlDB = require('./db/sqliteSetup');
 
 var passport = require('./config/passport');
 
@@ -22,23 +20,14 @@ var argv = parseArgs(process.argv.slice(2));
 var sqlDB = null;
 if (argv._.length == 0) {
   console.log("Setting up memory DB");
-  //sqlDB = require('./db/sqliteSetup');
   sqlDB = require('./db/sqliteSetup');
   app.locals.db = sqlDB;
 } else {
-  //console.log(sqlDB);
-
   app.locals.db = new sqlite3.Database(argv._[0]);
   console.log("File DB set up");
 }
 console.log(sqlDB);
 console.log("Comparison result: " + (app.locals.db == sqlDB));
-
-//app.locals.db = sqlDB;
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -48,10 +37,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(passport.initialize());
-
-/*var parseArgs = require('minimist');
-var argv = parseArgs(process.argv.slice(2));
-console.log(argv);*/
 
 app.use('/', routes);
 app.use('/swaprusers', users);
--- controllers/userController.js
+++ controllers/userController.js
@@ -50,35 +50,8 @@ module.exports.createUser = function(req, res) {
                           return res.status(201).send(response);
                       }
                   })
-                  //callback();
               });
           });
         }
     });
-    /*var user = {};
-    user["username"] = req.body.username;
-    user["name"] = req.body.name;
-    user["email"] = req.body.email;
-    bcrypt.genSalt(5, function(err, salt) {
-        if (err) return res.status(400).send({ "error": 'unable to create new user' });
-        bcrypt.hash(req.body.password, salt, null, function(err, hash) {
-            if (err) return callback(err);
-            user["password"]= hash;
-            user["token"] = jwt.sign(user, "app_secret");
-            req.app.locals.db.run("INSERT INTO id_map (username, email, full_name, pwd_hash, token, role_id) VALUES (?,?,?,?,?,?)", [req.body.username, req.body.email, req.body.name, user["password"], user["token"], 2], function(error) {
-                if (error) {
-                    return res.status(400).send({ "error": 'unable to create new user' });
-                } else {
-                    var response = {};
-                    response["id"] = this.lastID;
-                    response["username"] = req.body.username;
-                    response["name"] = req.body.name;
-                    response["email"] = req.body.email;
-                    response["role_id"] = 2;
-                    return res.status(201).send(response);
-                }
-            })
-            //callback();
-        });
-    });*/
 };

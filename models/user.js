b48ab9b1148104274d95a597fcb7b8b4bc579ab5
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2017-04-05 13:41:41 -0400

    put tighter constraints on the user model

--- models/user.js
+++ models/user.js
@@ -10,14 +10,38 @@ module.exports = function(sequelize, DataTypes) {
         'username': {
             'type': DataTypes.STRING,
             'allowNull': false,
-            'unique': true
+            'unique': true,
+            'validate': { 'notEmpty': true }
         },
-        'first_name': DataTypes.STRING,
-        'last_name': DataTypes.STRING,
-        'password': DataTypes.STRING,
-        'token': DataTypes.STRING,
+        'first_name': {
+            'type': DataTypes.STRING,
+            'allowNull': false,
+            'validate': { 'notEmpty': true }
+        },
+        'last_name': {
+           'type': DataTypes.STRING,
+            'allowNull': false,
+            'validate': { 'notEmpty': true }
+        },
+        'password': {
+          'type': DataTypes.STRING,
+            'allowNull': false,
+            'validate': { 'notEmpty': true }
+        },
+        'token': {
+          'type': DataTypes.STRING,
+            'allowNull': false,
+            'validate': { 'notEmpty': true }
+        },
         'role': DataTypes.STRING,
-        'email': DataTypes.STRING
+        'email': {
+          'type': DataTypes.STRING,
+          'allowNull': false,
+          'validate': { 
+              'notEmpty': true,
+              'isEmail': true
+          }
+        }
     });
 
     return User;

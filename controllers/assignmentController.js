8267b8ec6ec2962dff91140eea8b6dad969ee6b4
Ayan Das <ayandas@lawn-143-215-51-161.lawn.gatech.edu>
2017-04-17 16:42:28 -0400

    Changed the getActiveAssignments API to return all of a student's active assignments

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -1,20 +1,40 @@
 var models = require('../models');
 
 module.exports.getActiveAssignments = function(req, res) {
-    if (isNaN(req.body.sessionID)) {
-        return res.status(400).send({ 'error': 'Invalid session ID'});
+    if (isNaN(req.body.studentID)) {
+        return res.status(400).send({ 'error': 'Invalid student ID'});
     }
-    models.Assignment.findAll({ 'where': { 'SessionId': parseInt(req.body.sessionID) } })
+    models.SessionEnrollment.findAll({ 'where': { 'UserId': parseInt(req.body.studentID) } }).then(function(sessions) {
+        var sessionIDs = sessions.map(function(d) {
+            return d.SessionId;
+        });
+        models.Assignment.findAll({ 'where': { 'SessionId': { '$in': sessionIDs } } }).then(function(assignments) {
+            var today = new Date();
+            var activeAssignments = assignments.filter(function(d) {
+                return (d.openDate <= today && d.closeDate >= today);
+            }).map(function(d) {
+                return {
+                    "id": d.id,
+                    "openDate": d.openDate,
+                    "closeDate": d.closeDate,
+                    "ExerciseId": d.ExerciseId,
+                    "SessionId": d.SessionId
+                };
+            })
+            return res.status(201).send(activeAssignments);
+        }).catch(function(err) {
+            console.log(err);
+            return res.status(400).send({ 'error': 'Could not get the active assignments'});
+        });
+    }).catch(function(error) {
+        console.log(error);
+        return res.status(400).send({ 'error': 'Could not get the active assignments'});
+    });
+    /*models.Assignment.findAll({ 'where': { 'SessionId': parseInt(req.body.sessionID) } })
         .then(function(assignments) {
             console.log("Original length: " + assignments.length);
             var today = new Date();
             var activeAssignments = assignments.filter(function(a) {
-                /*console.log("Today: ");
-                console.log(today);
-                console.log("Start Date: ")
-                console.log(a.openDate);
-                console.log("End Date: ")
-                console.log(a.closeDate);*/
                 return (a.openDate <= today && a.closeDate >= today);
             }).map(function(a) {
                 return {
@@ -29,5 +49,5 @@ module.exports.getActiveAssignments = function(req, res) {
             return res.status(201).send(activeAssignments);
         }).catch(function(error) {
             return res.status(400).send({ 'error': 'Could not retrieve active assignments'});
-        })
+        })*/
 }

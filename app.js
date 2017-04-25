83312c73a2f3891fbc6a8aa8a48d7229f7dae19d
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2017-04-25 09:01:16 -0400

    cleanup

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-console.log(passport)
 app.use(passport.initialize());
 
 app.use('/', routes);

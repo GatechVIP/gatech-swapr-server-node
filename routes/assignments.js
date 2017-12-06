59747c6a9577adb7a72787354a52a6f0049c82ad
rgolding <golding.rh@gmail.com>
2017-12-06 14:04:09 -0500

    update to get active assignments

--- routes/assignments.js
+++ routes/assignments.js
@@ -2,24 +2,33 @@ var express = require('express');
 var router = express.Router();
 var assignmentController = require('../controllers/assignmentController');
 
-/* List ALL Assignments, no grades */
-/*router.route('/assignments')
-  .get();
+// /* List ALL Assignments, no grades */
+// router.route('/assignments')
+//   .get();
+// /* Make Submission Assignment */
+// router.route('/assignments')
+//   .post();
 
-/* Make Submission Assignment */
-/*router.route('/assignments')
-  .post();*/
+//  // Make Evaluation Assignment 
+// router.route('/evalassignments')
+//   .post();
 
-/* Make Evaluation Assignment */
-/*router.route('/evalassignments')
-  .post();*/
+// /* Retrieve an Assignment */
+// router.route('/assignments/:id')
+//   .get();
 
-/* Retrieve an Assignment */
-/*router.route('/assignments/:id')
-  .get();*/
+router.route('/assignments/active')
 
-router.route('/active')
-    .post(assignmentController.getActiveAssignments);
+    .post(function(req, res) {
+    	assignmentController.getActiveAssignments(req.body.studentID, 
+    		function(error, result) {
+    			if (error) {
+    				return res.status(error.status).send(error.message);
+    			}
+    			return res.send(result);
+    		})
+    	});
+    }
 
 
 module.exports = router;

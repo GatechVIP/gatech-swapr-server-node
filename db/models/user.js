9235caa51a5ceb311d48cab7d841e2e81696bba1
Brad Reardon <brad.jay.reardon@gmail.com>
2017-12-06 15:01:15 -0500

    Update length of token field on user

--- db/models/user.js
+++ db/models/user.js
@@ -31,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     },
     token: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(512),
         allowNull: false,
         validate: {
           notEmpty: true

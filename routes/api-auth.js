6a60b47d83d5cfa0c120f55b1b47fdf70cbc3a17
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2017-12-06 16:33:36 -0500

    fix token auth for non-local accounts

--- routes/api-auth.js
+++ routes/api-auth.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var authController = require('../controllers/authController')
+var authController = require('../controllers/authController');
+var passport = require('passport');
 
 /* Get Token */
-router.route('/')
-  .post(authController.getToken);
+router.route('/').post(passport.authenticate('token'), function(req, res) {
+    var sendRes = function(err, token) {
+        if (err) {
+            return res.status(err.status).send(err.message);
+        }
+        return res.send(token);
+    };
 
+    if (req.user) {
+        authController.getToken(req.user.username, null, sendRes);
+    } else {
+        authController.getToken(req.body.username, req.body.password, sendRes);
+    }
+});
 
 module.exports = router;
